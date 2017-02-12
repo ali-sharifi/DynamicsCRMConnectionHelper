70876c397c5ad5c89a265afa9398d2b1e1f637f5
Ali Sharifi <sharifi.ali@hotmail.com>
2017-02-12 15:45:51 -0800

    Files added

---
+++ CRMConnectionHelper/ManagedTokenOrganizationServiceProxy.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+
+namespace CRMConnectionHelper
+{
+    public sealed class ManagedTokenOrganizationServiceProxy : OrganizationServiceProxy
+    {
+        private AutoRefreshSecurityToken<OrganizationServiceProxy, IOrganizationService> _proxyManager;
+
+        public ManagedTokenOrganizationServiceProxy(Uri serviceUri, ClientCredentials userCredentials)
+            : base(serviceUri, null, userCredentials, null)
+        {
+            this._proxyManager = new AutoRefreshSecurityToken<OrganizationServiceProxy, IOrganizationService>(this);
+        }
+
+        public ManagedTokenOrganizationServiceProxy(IServiceManagement<IOrganizationService> serviceManagement,
+            SecurityTokenResponse securityTokenRes)
+            : base(serviceManagement, securityTokenRes)
+        {
+            this._proxyManager = new AutoRefreshSecurityToken<OrganizationServiceProxy, IOrganizationService>(this);
+        }
+
+        public ManagedTokenOrganizationServiceProxy(IServiceManagement<IOrganizationService> serviceManagement,
+            ClientCredentials userCredentials)
+            : base(serviceManagement, userCredentials)
+        {
+            this._proxyManager = new AutoRefreshSecurityToken<OrganizationServiceProxy, IOrganizationService>(this);
+        }
+
+        protected override void AuthenticateCore()
+        {
+            this._proxyManager.PrepareCredentials();
+            base.AuthenticateCore();
+        }
+
+        protected override void ValidateAuthentication()
+        {
+            this._proxyManager.RenewTokenIfRequired();
+            base.ValidateAuthentication();
+        }
+    }
+
+    /// <summary>
+    /// Class that wraps acquiring the security token for a service
+    /// </summary>
+    public sealed class AutoRefreshSecurityToken<TProxy, TService>
+        where TProxy : ServiceProxy<TService>
+        where TService : class
+    {
+        private TProxy _proxy;
+
+        /// <summary>
+        /// Instantiates an instance of the proxy class
+        /// </summary>
+        /// <param name="proxy">Proxy that will be used to authenticate the user</param>
+        public AutoRefreshSecurityToken(TProxy proxy)
+        {
+            if (null == proxy)
+            {
+                throw new ArgumentNullException("proxy");
+            }
+
+            this._proxy = proxy;
+        }
+
+        /// <summary>
+        /// Prepares authentication before authenticated
+        /// </summary>
+        public void PrepareCredentials()
+        {
+            if (null == this._proxy.ClientCredentials)
+            {
+                return;
+            }
+
+            switch (this._proxy.ServiceConfiguration.AuthenticationType)
+            {
+                case AuthenticationProviderType.ActiveDirectory:
+                    this._proxy.ClientCredentials.UserName.UserName = null;
+                    this._proxy.ClientCredentials.UserName.Password = null;
+                    break;
+                case AuthenticationProviderType.Federation:
+                case AuthenticationProviderType.LiveId:
+                    this._proxy.ClientCredentials.Windows.ClientCredential = null;
+                    break;
+                default:
+                    return;
+            }
+        }
+
+        /// <summary>
+        /// Renews the token (if it is near expiration or has expired)
+        /// </summary>
+        public void RenewTokenIfRequired()
+        {
+            if (null != this._proxy.SecurityTokenResponse &&
+            DateTime.UtcNow.AddMinutes(15) >= this._proxy.SecurityTokenResponse.Response.Lifetime.Expires)
+            {
+                try
+                {
+                    this._proxy.Authenticate();
+                }
+                catch (CommunicationException)
+                {
+                    if (null == this._proxy.SecurityTokenResponse ||
+                        DateTime.UtcNow >= this._proxy.SecurityTokenResponse.Response.Lifetime.Expires)
+                    {
+                        throw;
+                    }
+
+                    // Ignore the exception 
+                }
+            }
+        }
+    }
+}
---
+++ CRMConnectionHelper/XrmConnectionProvider.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+using Microsoft.Xrm.Sdk.Discovery;
+using System;
+using System.Net;
+using System.ServiceModel.Description;
+using System.Configuration;
+
+namespace CRMConnectionHelper
+{
+    public class XrmConnectionProvider
+    {
+        private static IOrganizationService instance;
+        private static object _lockObject = new object();
+
+        private static IConfigurationService _configurationService;
+
+        private XrmConnectionProvider() { }
+
+
+        public static IOrganizationService CRMService
+        {
+            get
+            {
+                try
+                {
+                    lock (_lockObject)
+                    {
+                        if (instance == null)
+                        {
+                            var container = IoC.Initialize();
+                            _configurationService = container.GetInstance<IConfigurationService>();
+
+
+                            instance = Connect();
+                        }
+                        return instance;
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    throw new Exception("Unable to connect to CRM", ex);
+                }
+
+            }
+        }
+        private static IOrganizationService Connect()
+        {
+            var config = _configurationService.Get<XrmClientConfiguration>();
+
+
+            Uri dInfo = new Uri(config.XrmUri);
+            ClientCredentials clientcred = new ClientCredentials();
+            clientcred.UserName.UserName = config.XrmClientCredUserName;
+            clientcred.UserName.Password = config.XrmClientCredPassword;
+
+
+            #region on-premise/online
+
+            DiscoveryServiceProxy dsp = new DiscoveryServiceProxy(dInfo, null, clientcred, null);
+            dsp.Authenticate();
+            RetrieveOrganizationsRequest rosreq = new Microsoft.Xrm.Sdk.Discovery.RetrieveOrganizationsRequest();
+            RetrieveOrganizationsResponse r = (RetrieveOrganizationsResponse)dsp.Execute(rosreq);
+
+            //get the OrganizationService
+            ManagedTokenOrganizationServiceProxy _serviceproxy = new ManagedTokenOrganizationServiceProxy(new Uri(config.XrmOrgServiceProxy), clientcred);
+            //In order to use the generated types when dealing with the organization service, you have to add the ProxyTypesBehavior to the endpoint Behaviors collection. This instructs the OrganizationServiceProxy to look in the assembly for classes with certain attributes to use. The generated classes are already attributed with these attributes. Simply, this makes all interactions with the organization service to be done using the generated typed classes for each entity instead of the generic Entity class we used earlier.
+            _serviceproxy.ServiceConfiguration.CurrentServiceEndpoint.EndpointBehaviors.Add(new ProxyTypesBehavior());
+            //Do not forget to include _serviceproxy.EnableProxyTypes();. Without this line,you will be unable to use early binding.
+            _serviceproxy.EnableProxyTypes();
+            IOrganizationService service = (IOrganizationService)_serviceproxy;
+
+            #endregion
+            return service;
+        }
+    }
+}
